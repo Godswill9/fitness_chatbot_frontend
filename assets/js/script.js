a50c273785cb00fb4ffc611b3fff81bf6932fe62
godswill9 <ghycinth9@gmail.com>
2023-11-24 06:22:19 -0800

    new commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,26 @@
-'use strict';
-
-
-
-
+"use strict";
 
 /**
  * PRELOAD
- * 
+ *
  * loading will be end after document is loaded
  */
 
 const preloader = document.querySelector("[data-preaload]");
 
+const chatBot = document.querySelector(".chatBot");
+const chatMain = document.querySelector(".chatMain");
 
+chatMain.style.height = "0px";
+chatMain.style.width = "0px";
+chatMain.style.visibility = "hidden";
+var state = "Closed";
 
 //Remove the pre-loader when the page has loaded
 window.addEventListener("load", () => {
   preloader.classList.add("loaded");
   document.body.classList.add("loaded");
-  
-})
-
-
+});
 
 /**
  * add event on element
@@ -35,9 +34,7 @@ const addEventOnElem = function (elem, type, callback) {
   } else {
     elem.addEventListener(type, callback);
   }
-}
-
-
+};
 
 /**
  * navbar toggle
@@ -47,29 +44,54 @@ const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const navLinks = document.querySelectorAll("[data-nav-link]");
 
-const toggleNavbar = function () { navbar.classList.toggle("active"); }
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+};
 
 addEventOnElem(navTogglers, "click", toggleNavbar);
 
-const closeNavbar = function () { navbar.classList.remove("active"); }
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+};
 
 addEventOnElem(navLinks, "click", closeNavbar);
-
-
 
 /**
  * header & back top btn active
  */
 
 const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
+// const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 100) {
     header.classList.add("active");
-    backTopBtn.classList.add("active");
+    // backTopBtn.classList.add("active");
   } else {
     header.classList.remove("active");
-    backTopBtn.classList.remove("active");
+    // backTopBtn.classList.remove("active");
+  }
+});
+
+chatBot.addEventListener("click", () => {
+  chatBot.classList.remove("animate");
+  if (state == "Closed") {
+    state = "Opened";
+    setTimeout(() => {
+      chatMain.style.visibility = "visible";
+      // chatMain.style.height = "31em";
+      chatMain.style.height = "";
+      chatMain.style.width = "";
+      // chatMain.src = "http://127.0.0.1:5500/frontend/";
+      chatMain.src = "https://fitness-bot.netlify.app/";
+    }, 100);
+  } else {
+    state = "Closed";
+    setTimeout(() => {
+      chatMain.style.visibility = "hidden";
+      chatMain.style.height = "0px";
+      chatMain.style.width = "0px";
+      chatMain.src = "";
+    }, 100);
   }
 });
